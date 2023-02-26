db55e083c9fd818b990d2ef0290f440e695b65f8
erikafazarah00 <erika.fazarah@gmail.com>
2023-02-26 03:39:19 -0800

    tamplate.php

---
+++ resources/views/city/index.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('title', 'City-Telnest')
+
+@section('main')
+    <main id="main" class="main">
+        <h1>HELLO WORLD!!!!</h1>
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
+
+Route::get('/city', [CityController::class, 'index']);
+Route::get('/create', [CityController::class, 'create']);
